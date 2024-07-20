0212f685df656e9e599c05a145ddfe80d1fab18e
SIDDHARTH GORE <siddharth.gore@live.com>
2024-07-20 18:04:16 +0530

    to do list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+
+
+  const [text, setText] = useState('')
+
+  const [options, setOptions] = useState([])
+
+
+  const handleText = (e) => {
+
+    console.log('handleText', e.target.value);
+    setText(e.target.value)
+  }
+
+
+  const handleAddOption = () => {
+    console.log('handleAddOption', text)
+
+
+
+    const tempOptions = options.concat(text);
+
+    setOptions(tempOptions)
+    setText('')
+
+  }
+
+
+  const handleRemoveAll = () => {
+
+    setOptions([])
+  }
+  const handleRemoveOption = (index) => {
+
+    console.log('handleRemoveOption', index)
+
+
+    const filteredOptions = options.filter((option, i) => i !== index)
+
+
+    console.log('filteredOptions', filteredOptions);
+
+    setOptions(filteredOptions)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <div className='container'>
+        <h1>To Do List</h1>
+
+
+        <button onClick={handleRemoveAll} className='remove-all' disabled={options.length === 0}>Remove All</button>
+        {options.map((option, index) => {
+          return (<div key={index} className='option'>
+
+            <p>{option}</p>
+
+            <button onClick={() => handleRemoveOption(index)} className='remove-option'>Remove</button>
+          </div>)
+        })}
+
+
+        <input type='text' onChange={handleText} value={text} />
+        <button onClick={handleAddOption} disabled={!text}>Add Option</button>
+      </div>
+
     </div>
   );
 }
